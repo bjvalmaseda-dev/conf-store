164f548532236be7a87abf588441960676ae56e3
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-24 16:01:32 -0400

    :sparkles: [ADD] routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './routes/App';
 
-const rootElement = document.getElementById("app");
+const rootElement = document.getElementById('app');
 const root = createRoot(rootElement);
 
 root.render(<App />);
